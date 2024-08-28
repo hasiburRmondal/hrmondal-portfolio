649c50a92acac1fbda5153540b6f9e225fc99beb
hasiburRmondal <hasibur959@gmail.com>
2024-08-28 17:09:01 +0530

    Portfolio

--- custom.js
+++ custom.js
@@ -1,29 +1,3 @@
-// Mobile menu toggle
-const menuToggle = document.querySelector('.menu-toggle');
-const navMenu = document.querySelector('nav ul');
-
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('show');
-});
-
-// Smooth scrolling for navigation
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// Contact form submission
-//document.getElementById('contact-form').addEventListener('submit', function (e) {
- //   e.preventDefault();
- //   alert('Your message has been sent!');
- //   this.reset(); // Clear the form fields
-// });
-
-// Project carousel
 $(document).ready(function () {
     // Initialize Owl Carousel
     $(".owl-carousel").owlCarousel({
@@ -43,5 +17,21 @@ $(document).ready(function () {
         }
     });
 
-});
+    // Mobile menu toggle
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navMenu = document.querySelector('nav ul');
 
+    menuToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show');
+    });
+
+    // Smooth scrolling for navigation
+    document.querySelectorAll('nav a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+});
